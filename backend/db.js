6bb90ba324c95e452a5c8fe1a79df175af8f3451
Your Name <you@example.com>
2025-11-21 12:15:28 +0100

    new app

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+// backend/db.js
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "calculator",
+  password: "your_password",
+  port: 5432,
+});
+
+module.exports = pool;
